31077596c5701cd578b618bf293bda75a6291595
hungtriinh <hungtrinh.se@gmail.com>
2021-04-01 23:20:53 +0700

    AccountManager | 4/1 | 23:19
    Create AccountManager.cshtml
    Update profile accounts had role==2 || role==3

--- Project PRN/Project PRN/Controllers/ManagerController.cs
+++ Project PRN/Project PRN/Controllers/ManagerController.cs
@@ -1,4 +1,5 @@
-﻿using Project_PRN.Models;
+﻿using PagedList;
+using Project_PRN.Models;
 using System;
 using System.Collections.Generic;
 using System.Data.Entity;
@@ -8,16 +9,21 @@ using System.Net.Mail;
 using System.Web;
 using System.Web.Mvc;
 
-namespace Project_PRN.Controllers {
-    public class ManagerController : Controller {
+namespace Project_PRN.Controllers
+{
+    public class ManagerController : Controller
+    {
         private ProjectPRNEntities3 db = new ProjectPRNEntities3();
-        public ActionResult ContactManager() {
+        public ActionResult ContactManager()
+        {
             return View();
         }
 
-        public JsonResult ContactJson() {
+        public JsonResult ContactJson()
+        {
             db.Configuration.ProxyCreationEnabled = false;
-            List<Contact> contacts = db.Contacts.ToList().Select(contact => new Contact {
+            List<Contact> contacts = db.Contacts.ToList().Select(contact => new Contact
+            {
                 userid = contact.userid,
                 email = contact.email,
                 content = contact.content,
@@ -29,69 +35,99 @@ namespace Project_PRN.Controllers {
             }).Where(c => c.status == false).ToList();
             return Json(contacts, JsonRequestBehavior.AllowGet);
         }
-        public ActionResult AdminBillManager(int? type, DateTime? date) {
+        public ActionResult AdminBillManager(int? type, DateTime? date)
+        {
             db.Configuration.ProxyCreationEnabled = false;
-            if (Session["user"] == null) {
+            if (Session["user"] == null)
+            {
                 return RedirectToAction("SignIn", "Accounts");
-            } else {
+            }
+            else
+            {
                 int userID = Int32.Parse(Session["user"].ToString());
                 Account account = db.Accounts.Find(userID);
-                if (account.role == 1) {
-                    if(type != null) {
+                if (account.role == 1)
+                {
+                    if (type != null)
+                    {
                         ViewData["type"] = type;
-                    } else {
+                    }
+                    else
+                    {
                         ViewData["type"] = 1;
                     }
-                    if (date != null) {
+                    if (date != null)
+                    {
                         ViewData["date"] = date;
-                    } else {
+                    }
+                    else
+                    {
                         ViewData["date"] = DateTime.Now;
                     }
                     return View();
-                } else {
+                }
+                else
+                {
                     return RedirectToAction("Index", "Home");
                 }
             }
         }
 
-        public ActionResult StaffBillManager() {
+        public ActionResult StaffBillManager()
+        {
             db.Configuration.ProxyCreationEnabled = false;
-            if (Session["user"] == null) {
+            if (Session["user"] == null)
+            {
                 return RedirectToAction("SignIn", "Accounts");
-            } else {
+            }
+            else
+            {
                 int userID = Int32.Parse(Session["user"].ToString());
                 Account account = db.Accounts.Find(userID);
-                if (account.role == 3) {
+                if (account.role == 3)
+                {
                     return View();
-                } else {
+                }
+                else
+                {
                     return RedirectToAction("Index", "Home");
                 }
             }
         }
 
-        public ActionResult BillManager() {
+        public ActionResult BillManager()
+        {
             db.Configuration.ProxyCreationEnabled = false;
-            if (Session["user"] == null) {
+            if (Session["user"] == null)
+            {
                 return RedirectToAction("SignIn", "Accounts");
-            } else {
+            }
+            else
+            {
                 int userID = Int32.Parse(Session["user"].ToString());
                 Account account = db.Accounts.Find(userID);
-                if (account.role == 3 || account.role == 2) {
+                if (account.role == 3 || account.role == 2)
+                {
                     return View();
-                } else {
+                }
+                else
+                {
                     return RedirectToAction("Index", "Home");
                 }
             }
         }
 
-        public ActionResult ReplyContact(int? contactId) {
+        public ActionResult ReplyContact(int? contactId)
+        {
             ViewData["contactId"] = contactId;
             return View();
         }
 
-        public JsonResult GetContactByID(int? contactId) {
+        public JsonResult GetContactByID(int? contactId)
+        {
             db.Configuration.ProxyCreationEnabled = false;
-            Contact ct = db.Contacts.ToList().Select(contact => new Contact {
+            Contact ct = db.Contacts.ToList().Select(contact => new Contact
+            {
                 userid = contact.userid,
                 email = contact.email,
                 content = contact.content,
@@ -106,15 +142,19 @@ namespace Project_PRN.Controllers {
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult ReplyContact(string email, string subject, string content, int? contactId) {
-            try {
-                if (ModelState.IsValid) {
+        public ActionResult ReplyContact(string email, string subject, string content, int? contactId)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
                     MailAddress senderEmail = new MailAddress("pes2020testing@gmail.com", "BookStore");
                     MailAddress receiverEmail = new MailAddress(email, "Receiver");
                     string password = "pes2020test";
                     string sub = subject;
                     string body = content;
-                    SmtpClient smtp = new SmtpClient {
+                    SmtpClient smtp = new SmtpClient
+                    {
                         Host = "smtp.gmail.com",
                         Port = 587,
                         EnableSsl = true,
@@ -122,11 +162,13 @@ namespace Project_PRN.Controllers {
                         UseDefaultCredentials = false,
                         Credentials = new NetworkCredential(senderEmail.Address, password)
                     };
-                    using (MailMessage mess = new MailMessage(senderEmail, receiverEmail) {
+                    using (MailMessage mess = new MailMessage(senderEmail, receiverEmail)
+                    {
 
                         Subject = subject,
                         Body = body
-                    }) {
+                    })
+                    {
                         mess.IsBodyHtml = true;
                         smtp.Send(mess);
                     }
@@ -137,43 +179,129 @@ namespace Project_PRN.Controllers {
                     db.SaveChanges();
                 }
 
-            } catch (Exception) {
+            }
+            catch (Exception)
+            {
                 ViewBag.Message = "Some Error";
                 return View();
             }
             return RedirectToAction("ContactManager");
         }
 
-        public ActionResult ChangeStatus(string sBillID, string sType) {
-            if (sType.Equals("0")) {
-                try {
+        public ActionResult ChangeStatus(string sBillID, string sType)
+        {
+            if (sType.Equals("0"))
+            {
+                try
+                {
                     long billId = Convert.ToInt64(sBillID);
                     List<Bill> bill = db.Bills.Where(b => b.BillID == billId).ToList();
-                    foreach (Bill b in bill) {
+                    foreach (Bill b in bill)
+                    {
                         b.status = 0;
                         db.Entry(b).State = EntityState.Modified;
                         db.SaveChanges();
                     }
                     return RedirectToAction("BillManager");
-                } catch {
+                }
+                catch
+                {
                     return null;
                 }
-            } else if (sType.Equals("1")) {
-                try {
+            }
+            else if (sType.Equals("1"))
+            {
+                try
+                {
 
                     long billId = Convert.ToInt64(sBillID);
                     List<Bill> bill = db.Bills.Where(b => b.BillID == billId).ToList();
-                    foreach (Bill b in bill) {
+                    foreach (Bill b in bill)
+                    {
                         b.status = 2;
                         db.Entry(b).State = EntityState.Modified;
                         db.SaveChanges();
                     }
-                } catch {
+                }
+                catch
+                {
                     return null;
                 }
             }
             return null;
+        }
 
+        public ActionResult AccountManager(int? page)
+        {
+            db.Configuration.ProxyCreationEnabled = false;
+            if (Session["user"] == null)
+            {
+                return RedirectToAction("SignIn", "Accounts");
+            }
+            else
+            {
+                int userID = Int32.Parse(Session["user"].ToString());
+                Account account = db.Accounts.Find(userID);
+                if (account.role == 1)
+                {
+                    if (page != null)
+                    {
+                        ViewData["page"] = page;
+                    }
+                    else
+                    {
+                        ViewData["page"] = 1;
+                    }
+                    return View();
+                }
+                else
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+        }
+
+        public JsonResult AccountJson(int? page)
+        {
+            db.Configuration.ProxyCreationEnabled = false;
+            List<Account> listAccount;
+            if (page == null)
+            {
+                page = 1;
+            }
+            int totalPage = 0;
+            int pageSize = 6;
+            int pageNumber = (page ?? 1);
+            listAccount = db.Accounts.ToList().Where(a => a.role > 1).Select(account => new Account
+            {
+                userID = account.userID,
+                email = account.email,
+                password = account.password,
+                userName = account.userName,
+                role = account.role,
+                address = account.address,
+                phoneNumber = account.phoneNumber,
+            }).OrderByDescending(product => product.role).ToPagedList(pageNumber, pageSize).ToList();
+            totalPage = db.Accounts.Where(a => a.role > 1).Count() / pageSize + 1;
+            return Json(new
+            {
+                totalPage = totalPage,
+                accountList = listAccount
+            }, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult AdminEditProfile([Bind(Include = "userID,email,password,userName,address,phoneNumber")] Account account)
+        {
+            Account a = db.Accounts.Find(account.userID);
+            a.email = account.email;
+            a.userName = account.userName;
+            a.address = account.address;
+            a.phoneNumber = account.phoneNumber;
+            db.Entry(a).State = EntityState.Modified;
+            db.SaveChanges();
+            return RedirectToAction("AccountManager");
         }
     }
 }
